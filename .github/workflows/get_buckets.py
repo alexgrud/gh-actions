155ab531bc08a1d5b72d8a278667ce44e04b471d
alexgrud <30340099+alexgrud@users.noreply.github.com>
2023-11-07 15:23:31 +0100

    Update get_buckets.py

--- .github/workflows/get_buckets.py
+++ .github/workflows/get_buckets.py
@@ -5,7 +5,28 @@ except ImportError:
     print("Cannot import modules")
     raise
 
+def listBuckets(client):
+    args = {}
+    buckets = []
+    try:
+        while True:
+            response = client.list_buckets(**args)
+            if response:
+                if response.get('Buckets', []):
+                    for bucket in response['Buckets']:
+                        buckets.append(bucket['Name'])
+            if 'NextToken' in response:
+                args['NextToken'] = response['NextToken']
+            else:
+                break
+        return buckets
+    except Exception as e:
+        print('Unable to list buckets with error: {}'.format(str(e)))
+        return []
+     
 if __name__ == 'main':
     region = 'eu-west-1'
-    client = boto3.client('s3', region_name=region)
+    s3Client = boto3.client('s3', region_name=region)
+    buckets = listBuckets(client=s3Client)
+    print('Buckets on account: {}'.format(buckets))
 
